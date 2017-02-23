7748c38434865382f1a98c1c1876b02a6d195cbd
Ravi Shekhar <ravi.shekhar@gmail.com>
2017-02-22 21:47:03 -0500

    Adding download of citibike data script.

---
+++ download_scripts/download-bike-data.sh
@@ -0,0 +1,1 @@
+cat urls_bike.txt | xargs -n 1 wget -P ./raw_data/bike
---
+++ download_scripts/make_directories.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p raw_data/bike
+mkdir -p raw_data/taxi
+mkdir -p raw_data/uber
+mkdir -p raw_data/subway
+
