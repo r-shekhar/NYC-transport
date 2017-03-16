5f04b9c89696abec7573dc735d08b2b600d29cd3
Ravi Shekhar <ravi.shekhar@gmail.com>
2017-03-15 21:01:17 -0400

    Subway conversion to Parquet seems like it's working. Still need to write tests.

--- 05_raw_to_dataframe/convert_subway_csv_to_parquet.py
+++ 05_raw_to_dataframe/convert_subway_csv_to_parquet.py
@@ -3,15 +3,15 @@
 from dask.distributed import Client
 from glob import glob
 
-import dask
+import pandas as pd
+from dask import delayed
 import dask.bag as db
-import dask.dataframe as dd
 import json
 import numpy as np
 import os
 import six
-import datetime
-import pprint
+import itertools
+from dateutil import parser
 
 columns = ('ca', 'unit', 'scp', 'station', 'linename', 'division',
            'endtime', 'description', 'cumul_entries', 'cumul_exits')
@@ -47,7 +47,7 @@ def parse_line(l):
         'C/A,UNIT,SCP,STATION,LINENAME,DIVISION,DATE,TIME,DESC,ENTRIES,EXITS'
 
     if (len(l) == 0) or (l == new_header_line):
-        return (None,)
+        return None
 
     l2 = l.split(',')
     Nfields = len(l2)
@@ -57,24 +57,16 @@ def parse_line(l):
         if (Nfields == 11):
             # new format
             values = list(l2)
+
+            # fields 7 and 8 contain the date and time respectively
+            # combine into one field and parse
+            values[6] = parser.parse("{} {}".format(*values[6:8])).isoformat()
+            values.pop(7)  # folded info into item 6
+
             for i in range(len(datatypes)):
-                # fields 7 and 8 contain the date and time respectively
-                # replace with a python datetime
-                values[6] = datetime.datetime.strftime(
-                    datetime.datetime.strptime("%s %s" %
-                                               (values[6], values[7]),
-                                               '%m/%d/%Y %H:%M:%S'),
-                    '%Y-%m-%d %H:%M:%S')
-                values.pop(7) # folded info into item 6
-
-                if datatypes[i] == 'text':
-                    continue
-                elif datatypes[i] == 'integer':
+                if datatypes[i] == np.int64:
                     values[i] = int(values[i])
-                else:
-                    # it should be ok to let time and text datatypes through
-                    pass
-
+            # assert(len(values) == 10)
             # values.insert(0, None)  # for the primary key
             RV = (tuple(values), )
 
@@ -88,87 +80,62 @@ def parse_line(l):
                 values = l2[0:3]
                 values.extend(['NULL', ]*3)
                 values.extend(t)
-                assert(len(values) == 11)
-                values[6] = datetime.datetime.strftime(
-                    datetime.datetime.strptime("%s %s" %
-                                               (values[6], values[7]),
-                                               '%m-%d-%y %H:%M:%S'),
-                    '%Y-%m-%d %H:%M:%S')
+                values[6] = parser.parse("{} {}".format(*values[6:8])).isoformat()
                 values.pop(7) # folded info into item 6
                 for i in range(len(datatypes)):
-                    if datatypes[i] == 'text':
-                        continue
-                    elif datatypes[i] == 'integer':
+                    if datatypes[i] == np.int64:
                         values[i] = int(values[i])
-                    else:
-                        # it should be ok to let time and text datatypes
-                        # through
-                        pass
-                # values.insert(0, None)
                 outdata.append(tuple(values))
             RV = tuple(outdata)
         else:
             # Format not recognized
             # print so output can be looked at manually
-            print(l)
+            # print(l)
             RV = None
     except Exception as e:
+        print(e)
         RV = None
         pass
 
     return RV
 
+def parse_single_file(filename):
+    with open(filename) as fh:
+        d = filter(lambda l: not (l is None), map(parse_line, fh.readlines()), )
+        d = list(itertools.chain(*d))
 
-def main(filelist):
-    b = db.read_text(filelist).str.strip().map(parse_line)
-    # b = b.concat().filter(lambda l: (l is not None) and (len(l) == 10))
-    # b = b.compute(get=dask.async.get_sync)
+        df = pd.DataFrame(d, columns=columns)
+        print(df.shape)
+        print( '{} : {}'.format(filename, len(d)))
+        return d
 
+def main(files):
 
-    # b.to_textfiles(os.path.join(config['parquet_output_path'], 'junk/*.txt'))
-    
-    df = b.to_dataframe(columns=columns)
-    # df = df.compute()
-    # print(df)
-    df = df.repartition(npartitions=50)
-    df['cumul_exits'] = df.cumul_exits.astype(np.float64)
-    df['cumul_entries'] = df.cumul_entries.astype(np.float64)
-    df['endtime'] = df.endtime.astype('M8[s]')
+    bag = db.from_delayed([delayed(parse_single_file)(fn) for fn in files])
+    df = bag.to_dataframe(columns=columns)
 
-    dd.to_parquet(os.path.join(config['parquet_output_path'], 'subway.parquet'),
-        df, compression='SNAPPY', object_encoding='json')
+    df['endtime'] = df['endtime'].astype(np.datetime64)
+    df['endtime'] = (df.endtime.astype(np.int64) // 1e9).astype(np.int64)
 
-def load_raw_into_sql(filelist):
-    "Parse csv files, load data"
+    df.cumul_entries.astype(np.int64)
+    df.cumul_exits.astype(np.int64)
 
-    # Ideally, pandas.read_csv would do this in seconds. Unfortunately this
-    # data has quite a few (approximately ~5000) formatting errors and a change
-    # in format mid-file. So must do it manually.
+    # df = df.categorize()
+    # fastparquet.write(os.path.join(config['parquet_output_path'], 'subway.parquet'), df,
+    #                   compression='SNAPPY', object_encoding='json')
 
-    # chains together the files into a single iterable
-    with fileinput.input(files=filelist) as f:
-        for l in f:
-            RV = parse_line(l)
-
-            if not (RV is None):
-                insert_query = '''INSERT INTO entries (ca, unit, scp, station, 
-                   linename, division, endtime, description, 
-                   cumul_entries, cumul_exits) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);'''
-                for r in RV:
-                    try:
-                        pg_cur.execute(insert_query, r)
-                    except ValueError as v:
-                        sys.stderr.write(r)
-                        sys.stderr.write(v)
-                        sys.stderr.write(os.linesep)
+    # df = df.repartition(npartitions=50)
+    df.to_parquet(os.path.join(config['parquet_output_path'], 'subway.parquet'),
+                  compression="SNAPPY", object_encoding='json'
+                  )
 
 
 if __name__ == '__main__':
-    # client = Client()
-    # load_raw_into_sql(get_raw_data_files())
-
+    client = Client()
     files = sorted(
             glob(os.path.join(config["subway_raw_data_path"],
                               'turnstile*.txt')))
+
+    # parse_single_file('../00_download_scripts/raw_data/subway/turnstile_150328.txt')
     main(files)
 
