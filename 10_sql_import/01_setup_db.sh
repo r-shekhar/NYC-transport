7a3d351b64b7332d56a823f3e2f29cf01575859c
Ravi Shekhar <ravi.shekhar@gmail.com>
2017-04-21 11:56:09 -0400

    Exploratory work shows the assign_taxi_zones function has a bug I cannot track down.

--- 10_sql_import/01_setup_db.sh
+++ 10_sql_import/01_setup_db.sh
@@ -18,5 +18,17 @@ create user transituser;
 \c transitproject
 grant all privileges on all tables in schema public to transituser;
 alter role transituser with encrypted password 'yourpassword';
+CREATE extension postgis;
+CREATE extension cstore_fdw;
 EOF
 
+# shp2pgsql -s 2263:4326 ../shapefiles/nyct2010.shp | psql -d transitproject
+shp2pgsql -s 2263:4326 ../shapefiles/taxi_zones.shp | psql -d transitproject
+
+psql -d transitproject<<EOF
+CREATE INDEX idx_taxi_zones_geom ON taxi_zones USING gist (geom);
+CREATE INDEX idx_taxi_zone_id ON taxi_zones (locationid);
+
+grant all privileges on all tables in schema public to transituser;
+GRANT SELECT ON public.geometry_columns TO transituser;
+EOF
