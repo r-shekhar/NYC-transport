ed48881364009a109cc2e9634ada5ff3c040ff09
Ravi Shekhar <ravi.shekhar@gmail.com>
2017-04-29 22:13:38 -0400

    Edited spatial merge for bikes data so it doesnt conflict with taxi spatial merge.

--- 10_sql_import/04_spatial_updates_bike.sh
+++ 10_sql_import/04_spatial_updates_bike.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql `cat ~/.sqlconninfo` <<EOF
-CREATE TABLE tmp_points AS
+CREATE TABLE tmp_points2 AS
 SELECT
   biketrip_id,
   ST_SetSRID(ST_MakePoint(start_station_longitude, start_station_latitude), 4326) as start_station,
@@ -17,40 +17,40 @@ ORDER BY biketrip_id;
 EOF
       
 
-psql `cat ~/.sqlconninfo` -c "CREATE INDEX idx_tmp_points_start ON tmp_points USING gist (start_station);" 
-psql `cat ~/.sqlconninfo` -c "CREATE INDEX idx_tmp_points_end ON tmp_points USING gist (end_station);" 
+psql `cat ~/.sqlconninfo` -c "CREATE INDEX idx_tmp_points2_start ON tmp_points2 USING gist (start_station);" 
+psql `cat ~/.sqlconninfo` -c "CREATE INDEX idx_tmp_points2_end ON tmp_points2 USING gist (end_station);" 
 wait 
 
 psql `cat ~/.sqlconninfo` <<EOF &
-CREATE TABLE tmp_starts_ct AS
+CREATE TABLE tmp_starts_ct2 AS
 SELECT t.biketrip_id, n.gid as start_ct_id
-FROM tmp_points t, nyct2010 n
+FROM tmp_points2 t, nyct2010 n
 WHERE ST_Within(t.start_station, n.geom);
-CREATE INDEX on tmp_starts_ct(biketrip_id);
+CREATE INDEX on tmp_starts_ct2(biketrip_id);
 EOF
 
 psql `cat ~/.sqlconninfo` <<EOF &
-CREATE TABLE tmp_starts_tz AS
+CREATE TABLE tmp_starts_tz2 AS
 SELECT t.biketrip_id, n.gid as start_taxizone_id
-FROM tmp_points t, taxi_zones n
+FROM tmp_points2 t, taxi_zones n
 WHERE ST_Within(t.start_station, n.geom);
-CREATE INDEX on tmp_starts_tz(biketrip_id);
+CREATE INDEX on tmp_starts_tz2(biketrip_id);
 EOF
 
 psql `cat ~/.sqlconninfo` <<EOF &
-CREATE TABLE tmp_ends_ct AS
+CREATE TABLE tmp_ends_ct2 AS
 SELECT t.biketrip_id, n.gid as end_ct_id
-FROM tmp_points t, nyct2010 n
+FROM tmp_points2 t, nyct2010 n
 WHERE ST_Within(t.end_station, n.geom);
-CREATE INDEX on tmp_ends_ct(biketrip_id);
+CREATE INDEX on tmp_ends_ct2(biketrip_id);
 EOF
 
 psql `cat ~/.sqlconninfo` <<EOF &
-CREATE TABLE tmp_ends_tz AS
+CREATE TABLE tmp_ends_tz2 AS
 SELECT t.biketrip_id, n.gid as end_taxizone_id
-FROM tmp_points t, taxi_zones n
+FROM tmp_points2 t, taxi_zones n
 WHERE ST_Within(t.end_station, n.geom);
-CREATE INDEX on tmp_ends_tz(biketrip_id);
+CREATE INDEX on tmp_ends_tz2(biketrip_id);
 EOF
 
 wait
@@ -83,19 +83,19 @@ OPTIONS(compression 'pglz');
 
 INSERT INTO bike_t 
   SELECT * FROM bike_ingest_col
-  FULL OUTER JOIN tmp_starts_ct USING (biketrip_id)
-  FULL OUTER JOIN tmp_starts_tz USING (biketrip_id)
-  FULL OUTER JOIN tmp_ends_ct USING (biketrip_id)
-  FULL OUTER JOIN tmp_ends_tz USING (biketrip_id)
+  FULL OUTER JOIN tmp_starts_ct2 USING (biketrip_id)
+  FULL OUTER JOIN tmp_starts_tz2 USING (biketrip_id)
+  FULL OUTER JOIN tmp_ends_ct2 USING (biketrip_id)
+  FULL OUTER JOIN tmp_ends_tz2 USING (biketrip_id)
   ORDER BY start_station_id, start_time;
 EOF
 
 psql `cat ~/.sqlconninfo` <<EOF
-DROP TABLE tmp_points CASCADE;
-DROP TABLE tmp_starts_ct CASCADE;
-DROP TABLE tmp_starts_tz CASCADE;
-DROP TABLE tmp_ends_ct CASCADE;
-DROP TABLE tmp_ends_tz CASCADE;
+DROP TABLE tmp_points2 CASCADE;
+DROP TABLE tmp_starts_ct2 CASCADE;
+DROP TABLE tmp_starts_tz2 CASCADE;
+DROP TABLE tmp_ends_ct2 CASCADE;
+DROP TABLE tmp_ends_tz2 CASCADE;
 DROP FOREIGN TABLE bike_ingest_col;
 EOF
 
